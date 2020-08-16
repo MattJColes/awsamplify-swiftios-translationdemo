c03566db16767e64cd98e8f121d1647fdbfef47c
Matt Coles <mattjcoles@outlook.com>
2020-08-16 13:18:55 +1000

    Initial Commit

---
+++ translationdemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  translationdemo
+//
+//  Created by Coles, Matt on 16/8/20.
+//  Copyright © 2020 Coles, Matt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
